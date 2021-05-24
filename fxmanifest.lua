627f49f71ac276753bfd9bb99383b43ffa933774
gubriansky@sernato.sk <gubriansky@sernato.sk>
2021-05-24 19:14:22 +0200

    seat icons and trying to fix not being able to swap seats

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -16,6 +16,8 @@ files {
     'ui/images/icons/048-car-door-r-down.png',
     'ui/images/icons/048-car-door-l-up.png',
     'ui/images/icons/048-car-door-r-up.png',
+    'ui/images/icons/051-seat.png',
+    'ui/images/icons/051-seat_red.png',
     'ui/sounds/seat-belt-in.ogg',
     'ui/sounds/seat-belt-out.ogg',
     'ui/config.js',
