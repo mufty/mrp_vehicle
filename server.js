425355ed875040b70693bea6dbb32f50dacb3175
gubriansky@sernato.sk <gubriansky@sernato.sk>
2021-05-26 21:20:39 +0200

    replace exports for shared object to keep unified

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+MRP_SERVER = null;
+
+emit('mrp:getSharedObject', obj => MRP_SERVER = obj);
+
+while (MRP_SERVER == null) {
+    print('Waiting for shared object....');
+}
+
 const ObjectId = require('mongodb').ObjectId;
 
 onNet('mrp:vehicle:save', (source, props) => {
-    exports["mrp_core"].DBCreate('vehicle', props, () => {
+    MRP_SERVER.create('vehicle', props, () => {
         exports["mrp_core"].log('Vehicle created!');
     });
 });
 
 RegisterCommand('vehLoad', (source, args, rawCommand) => {
     let id = ObjectId("60ae41eb77b7443a74b2c771");
-    exports["mrp_core"].DBRead('vehicle', id, (obj) => {
+    MRP_SERVER.read('vehicle', id, (obj) => {
         exports["mrp_core"].log(obj);
         emitNet('mrp:vehicle:applyProps', source, obj);
     });
