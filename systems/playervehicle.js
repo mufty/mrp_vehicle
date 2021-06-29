0ea69a0127942e8bd624b4d1cfe20fbcfb974589
mufty <gubriansky@sernato.sk>
2021-06-29 21:22:23 +0200

    lock NPC vehicles

--- systems/playervehicle.js
+++ systems/playervehicle.js
@@ -33,4 +33,18 @@ setInterval(() => {
     } else {
         isPlayerInVehicle = false;
     }
+
+    if (config.carlock.lockVehicles) {
+        //lock spawned NPC vehicles
+        let veh = GetVehiclePedIsTryingToEnter(ped);
+        if (DoesEntityExist(veh) && !IsVehiclePreviouslyOwnedByPlayer(veh)) {
+            let lock = GetVehicleDoorLockStatus(veh);
+            if (lock == 7)
+                SetVehicleDoorsLocked(veh, 2);
+
+            let pedd = GetPedInVehicleSeat(veh, -1);
+            if (pedd)
+                SetPedCanBeDraggedOut(pedd, false);
+        }
+    }
 }, 0);
